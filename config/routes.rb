afc65ba8ff78109a1abeb4d91964d5c06e4cc74e
Tina Liu <tina.chang.liu@gmail.com>
2019-06-03 14:09:59 +0900

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :plots, only: [:index, :show] do
+    resources :shortlists, only: [:create]
+  end
+
+  resources :shortlists, only: [:destroy] do
+    resources :comments, only: [:create]
+  end
+
+  resources :comments, only: [:destroy]
+
+  get 'profile', to: 'profile#show', as: 'profile'
 end
