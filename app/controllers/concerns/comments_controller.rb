8f261fa9b6a8c18e3382053ff834f8e46ad25342
Matthew Stuewe <stuewe.matthew@gmail.com>
2019-06-03 16:07:42 +0900

    Working on controllers

--- app/controllers/concerns/comments_controller.rb
+++ app/controllers/concerns/comments_controller.rb
@@ -1,5 +1,19 @@
 class CommentsController
 
 
+  def create
+
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+  end
+
+  private
+
+  def comment_params
+
+  end
+
 end
 
--- app/controllers/shortlists_controller.rb
+++ app/controllers/shortlists_controller.rb
@@ -3,13 +3,18 @@ class ShortlistsController < ApplicationController
 
 
   def create
-
+    @shortlist = Shortlist.new(shortlist_params)
+    authorize @shortlist
+    @shortlist.user = current_user
+    if @shortlist.save
   end
 
 
   def destroy
     @shortlist = Shortlist.find(params[:id])
-
+    autorize @shortlist
+    @shortlist.destroy
+    redirect_to profile_path
   end
 
 end
