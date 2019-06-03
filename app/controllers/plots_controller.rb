714cb2bda62b40a642b7886800f73dd0ed5b26aa
Matthew Stuewe <stuewe.matthew@gmail.com>
2019-06-03 16:01:07 +0900

    controller work

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -1,4 +1,13 @@
 class PlotsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show, :index]
 
+  def index
+    @plots = policy_scope(Plot).order
+  end
+
+  def show
+    @plot = Plot.find(params[:index])
+    authorize @plot
+  end
 
 end
--- app/controllers/shortlists_controller.rb
+++ app/controllers/shortlists_controller.rb
@@ -1,3 +1,15 @@
 class ShortlistsController < ApplicationController
 
+
+
+  def create
+
+  end
+
+
+  def destroy
+    @shortlist = Shortlist.find(params[:id])
+
+  end
+
 end
