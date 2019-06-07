96700ed4bd614be93b2783736ff532bbfb0831ff
Tina Liu <tina.chang.liu@gmail.com>
2019-06-07 12:45:48 +0900

    added back button and flash msg disappear after 2s

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,4 +21,10 @@ document.querySelector("#pills-map-tab").addEventListener("click", (event) => {
 
 })
 
-
+// dismiss alerts after 2 seconds
+const alertDismissBtn = document.querySelector('.alert button');
+if (alertDismissBtn) {
+  setTimeout(() => {
+    alertDismissBtn.click();
+  }, 2000);
+}
