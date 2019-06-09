6b8778a78b157ef1bd49adb44ec518fa3190190d
Tina Liu <tina.chang.liu@gmail.com>
2019-06-09 12:45:59 +0900

    added model validation

--- app/models/architect.rb
+++ app/models/architect.rb
@@ -1,3 +1,6 @@
 class Architect < ApplicationRecord
   has_many :appointments
+
+  validates :name, :website_url, :bio, :portfolio_url, :speciality, :min_project_budget, :appt_rate, presence: true
+  validates :min_project_budget, :appt_rate, numericality: { only_integer: true }
 end
