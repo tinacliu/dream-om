6df63701146ba3bb69ed78d1e5e44ad3725ffa18
Matthew Stuewe <stuewe.matthew@gmail.com>
2019-06-10 15:04:05 +0900

    initial commit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,14 +4,28 @@ class ProjectsController < ApplicationController
     @projects = policy_scope(Project)
   end
 
-
   def show
     @project = Project.find(params[:id])
     authorize @project
   end
 
   def create
+    @project = Project.new(project_params)
+    authorize @project
+    if @project.save
+      flash[:notice] = "Project successfully created"
+    else
+      flash[:alert] = "Project could not be saved"
+    end
+    redirect_to profile_path
 
   end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:brief, :title, :budget, :category, :plot_postcode)
+  end
+
 
 end
