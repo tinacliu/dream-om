0f195bd91ea87946220a7c243996c55bd7152be7
Matthew Stuewe <stuewe.matthew@gmail.com>
2019-06-12 10:30:51 +0900

    fixed comments, hopefully

--- app/models/shortlist.rb
+++ app/models/shortlist.rb
@@ -1,6 +1,6 @@
 class Shortlist < ApplicationRecord
   belongs_to :user
   belongs_to :plot
-  has_one :comment, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :photos, dependent: :destroy
 end
