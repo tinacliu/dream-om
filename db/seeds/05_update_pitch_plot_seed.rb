fa13d148139c3a4c53e7e38257991eb19247baf9
Tina Liu <tina.chang.liu@gmail.com>
2019-06-13 18:17:09 +0900

    minor styling

--- db/seeds/05_update_pitch_plot_seed.rb
+++ db/seeds/05_update_pitch_plot_seed.rb
@@ -1,7 +1,7 @@
-puts 'Updating the plot we are using on the pitch...'
-puts 'We will add images and add the plot size, please wait....'
-pitch_plot = Plot.where(listing_id: '51002424')
-pitch_plot[0].image_urls << 'https://lc.zoocdn.com/3f8d8e7e4899156a2b7f67e880e67ec33b974b11.jpg'
-pitch_plot[0].description << ', and is around 0.5 acres in size'
-pitch_plot[0].plot_size = '0.5 acres'
-pitch_plot[0].save
+# puts 'Updating the plot we are using on the pitch...'
+# puts 'We will add images and add the plot size, please wait....'
+# pitch_plot = Plot.where(listing_id: '51002424')
+# pitch_plot[0].image_urls << 'https://lc.zoocdn.com/3f8d8e7e4899156a2b7f67e880e67ec33b974b11.jpg'
+# pitch_plot[0].description << ', and is around 0.5 acres in size'
+# pitch_plot[0].plot_size = '0.5 acres'
+# pitch_plot[0].save
